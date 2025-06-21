2a69750e65ce2b0facb27d4773cd0f1f6080848a
Namir50 <namirsayyed50@gmail.com>
2025-06-21 16:23:40 +0530

    New

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template,redirect, url_for, session
 import pickle
 import numpy as np
 import pandas as pd
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
 app = application
+app.secret_key = 'hello'
 
 model = pickle.load(open('model/Netflix Stock Prediction Model.pkl','rb'))
 scaler = pickle.load(open('model/Scaler model.pkl','rb'))
@@ -25,9 +26,15 @@ def predictdatapoint():
         day = int(request.form.get('day'))
         
         scaled_data = scaler.transform([[open,high,low,year,month,day]])
-        result = model.predict(scaled_data)
+        result = round(model.predict(scaled_data)[0], 2)
         
-        return render_template('home.html', result = result[0])
+        session['result'] = result
+        return redirect(url_for('predictdatapoint'))
+        
+    result = session.pop('result','')
+    return render_template('home.html',result = result)
+
+    
     
 
 if __name__ == '__main__':
